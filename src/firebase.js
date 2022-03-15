421b6a104f905cdf4eb7bf41df99942f74601453
Mason Secky-Koebel <mseckykobel@gmail.com>
2022-03-15 09:22:50 -0500

    firebase hosting is working now

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAQpbt9ZZi-mpU5cTpx3gE9tRPW2Tikh8I",
-  authDomain: "ukraine-dashboard.firebaseapp.com",
-  projectId: "ukraine-dashboard",
-  storageBucket: "ukraine-dashboard.appspot.com",
-  messagingSenderId: "269602539853",
-  appId: "1:269602539853:web:c5e563eb73eca8a7d84f04",
-  measurementId: "G-WRRBHFXWNN",
+  apiKey: "AIzaSyDxuxn9S8Prl2lD0JoKhevNVLFLqu-mzys",
+  authDomain: "ukraine-dashboard-app.firebaseapp.com",
+  projectId: "ukraine-dashboard-app",
+  storageBucket: "ukraine-dashboard-app.appspot.com",
+  messagingSenderId: "88213970998",
+  appId: "1:88213970998:web:6c07c7d3845bc2ee4ab897",
+  measurementId: "G-KZFRLXGSBE",
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
