a9ed3e693e97dd634b482450cbe8b671f66db2c1
Mason Secky-Koebel <mseckykoebel@gmail.com>
2022-03-21 09:20:51 -0500

    building a test puppeteer script to do something with my website

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,40 @@
 import * as functions from "firebase-functions";
 import * as puppeteer from "puppeteer";
+import * as admin from "firebase-admin";
+
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+const options = { memory: "2GB", timeoutSeconds: 300 };
+
+export const findName = async () => {
+  const url = "https://www.masonseckykoebel.com";
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1920, height: 1080 });
+  await page.setRequestInterception(true); // not sure what this does yet
+  page.on("request", (req) => {
+    if (
+      req.resourceType() == "stylesheet" ||
+      req.resourceType() == "font" ||
+      req.resourceType() == "image"
+    ) {
+      req.abort();
+    } else {
+      req.continue();
+    }
+  });
+
+  await page.goto(url, { waitUntil: "networkidle2" });
+
+  const data = await page.evaluate(() => {
+      // TODO: do something
+      // const first
+  })
+};
 
 export const scrapeWebsiteDom = functions
   .runWith({ memory: "1GB" })
